ba04e60088929e35ab4ec409b700d500a78360f3
stHanUken <50410251+stHanUken@users.noreply.github.com>
2019-05-09 21:17:14 +0800

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,8 +1,16 @@
 #ifndef __STUDENT_H__
 #define __STUDENT_H__
 
-class Student {
+enum Gender {
+	MALE, FEMALE;
+};
 
+class Student {
+private:
+	enum Gender Sex;
+	int age;
+	
+public:
 };
 
 #endif
