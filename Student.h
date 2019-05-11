270a1b542c9281a2c51a2487001cc51ce42b8093
stHanUken <50410251+stHanUken@users.noreply.github.com>
2019-05-12 07:55:03 +0800

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,16 +1,38 @@
+#include <string>
+
 #ifndef __STUDENT_H__
 #define __STUDENT_H__
 
 enum Gender {
-	MALE, FEMALE;
+	MALE, FEMALE
 };
+
+enum Grade {
+	PRIMARY, JUNIOR, SENIOR
+};
+
+enum Mood {
+	EXCITED, HAPPY, MODERATE, NERVOUS, LOW, SAD, MAD, CRAZY
+};
+
+typedef struct {
+	int year;
+	int month;
+	int day;
+} Date;
 
 class Student {
 private:
 	enum Gender Sex;
-	int age;
-	
+	Date Birthday;
+	unsigned int Age;
+	enum Grade Level;
+	int Study_Hours;
+	enum Mood Now_Mood;
 public:
+	Student(enum Gender gender, Date day, unsigned int age, enum Grade level);
+	void Study(int hours);
+	void Complain(string str, int times);
 };
 
 #endif
