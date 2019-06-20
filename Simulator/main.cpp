98fcee0d6d263e9330bcb801ee760cab147348f6
stHanUken <1416759930@qq.com>
2019-06-20 10:18:04 +0800

    .

--- Simulator/main.cpp
+++ Simulator/main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:Simulator/main.cpp
 #include <iostream>
 #include "Settings.h"
 #include "Student.h"
@@ -11,17 +10,3 @@ int main()
 	return 0;
 }
 
-=======
-#include <iostream>
-#include "Settings.h"
-#include "Student.h"
-#include "Class.h"
-#include "School.h"
-
-int main()
-{
-
-	return 0;
-}
-
->>>>>>> 462f118689e7d23d15ac0e8b71757c3a0a093fc9:Emulator/main.cpp
